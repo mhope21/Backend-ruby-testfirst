c247789949aeb62474a95213921f812fd3d72a26
Marcia Hope <121902305+mhope21@users.noreply.github.com>
2024-07-17 15:47:51 -0500

    Update 01_temperature/temperature.rb ctof
    Co-authored-by: Raquel Román-Rodriguez <82777181+raquii@users.noreply.github.com>

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -6,9 +6,5 @@ end
 
 # converts temps from c to f
 def ctof(temp)
-  return 32 if temp == 0
-  return 212 if temp == 100
-  return 98.6 if temp == 37
-  return 68 if temp == 20
   (temp * 9.0 / 5.0) + 32
 end
